55f7147d18ad52e5133896e08c0ae8357b2db766
minhfli <vuvanminh.bacninh@gmail.com>
2025-04-02 17:14:14 +0700

    scripts

--- metrics/monitor-group.sh
+++ metrics/monitor-group.sh
@@ -1,50 +1,46 @@
 #!/bin/bash
 
-# simple script to monitor the CPU and memory usage of an OpenStack instance
+SERVER1=8fc212e7-f359-45bd-b0ba-4515a13c35bd
+SERVER2=60984c4d-ad79-4bd6-9c1d-15b25ef40ae7
 
-# Default OpenStack Resource ID (Can be overridden by command-line argument)
-DEFAULT_RESOURCE_ID="d27ee539-3d00-4bbe-9b3b-2cbaf5aff071"
+STACK_ID=eca1f527-4634-46c8-b526-73583225f38b
 
-# change the default resource id or call the script with the resource id
-RESOURCE_ID="${1:-$DEFAULT_RESOURCE_ID}"
-
-
-# Log files
-MEMORY_LOG="memory.log"
-CPU_LOG="cpu.log"
-CPU_util_p_LOG="cpu_util_percentage.log"
-CPU_util_n_LOG="cpu_util_nanosecond.log"
-
-# mkdir if not exist
-
-
-SERVER_NAME=$(openstack server show $RESOURCE_ID -f value -c name) 
-if [ -z "$SERVER_NAME" ]; then
-    echo "Error: Unable to retrieve server name for resource ID $RESOURCE_ID."
-    exit 1
-fi
-mkdir -p $SERVER_NAME
-CURRENT_DATE="$(date +%Y-%m-%d)"
-
-mkdir -p ./$SERVER_NAME/$CURRENT_DATE
+mkdir -p ./stack0
 
 while true; do
-    echo "=== $(date) ===" > ./$SERVER_NAME/$CURRENT_DATE/$MEMORY_LOG
-    openstack metric measures show --utc --resource-id "$RESOURCE_ID" memory.usage >> ./$SERVER_NAME/$CURRENT_DATE/$MEMORY_LOG
 
-    echo "=== $(date) ===" > ./$SERVER_NAME/$CURRENT_DATE/$CPU_LOG
-    openstack metric measures show --utc --resource-id "$RESOURCE_ID" cpu >> ./$SERVER_NAME/$CURRENT_DATE/$CPU_LOG
+    echo "logging memory usage for each server"
+    # logging ram usage for each server
+    echo "server1"
+    openstack metric measures show --utc --resource-id $SERVER1 memory.usage >./stack0/server1_memory.log
+    echo "server2"
+    openstack metric measures show --utc --resource-id $SERVER2 memory.usage >./stack0/server2_memory.log
 
-    echo "=== $(date) ===" > ./$SERVER_NAME/$CURRENT_DATE/$CPU_util_p_LOG
-    openstack metric aggregates --resource-type instance                      \
+    # openstack metric measures show --utc --resource-id "$SERVER1" cpu >./stack0/server1_cpu.log
+    # openstack metric measures show --utc --resource-id "$SERVER2" cpu >./stack0/server2_cpu.log
+
+    echo "logging cpu usage for each server"
+    # logging cpu usage for each server
+    echo "server1"
+    openstack metric aggregates --resource-type instance \
         "(* ( / (aggregate rate:mean (metric cpu mean)) 300000000000.0) 100)" \
-        "id=$RESOURCE_ID" >> ./$SERVER_NAME/$CURRENT_DATE/$CPU_util_p_LOG
+        "id=$SERVER1" >./stack0/server1_cpu_util_p.log
+    echo "server2"
+    openstack metric aggregates --resource-type instance \
+        "(* ( / (aggregate rate:mean (metric cpu mean)) 300000000000.0) 100)" \
+        "id=$SERVER2" >./stack0/server2_cpu_util_p.log
 
-    echo "=== $(date) ===" > ./$SERVER_NAME/$CURRENT_DATE/$CPU_util_n_LOG
-    openstack metric aggregates --resource-type instance                      \
-        "(aggregate rate:mean (metric cpu mean))" \
-        "id=$RESOURCE_ID" >> ./$SERVER_NAME/$CURRENT_DATE/$CPU_util_n_LOG
+    echo "logging cpu usage for the stack"
+    # logging cpu usage for the both
+    openstack metric aggregates --resource-type instance \
+        "(* ( / (aggregate rate:mean (metric cpu mean)) 300000000000.0) 100)" \
+        "server_group=$STACK_ID" >./stack0/servers_cpu_util_p.log
+
+    openstack metric aggregates --resource-type instance \
+        "(aggregate mean (metric cpu mean))" \
+        "server_group=$STACK_ID" >./stack0/servers_memory.log
 
     echo "Metrics logged. Sleeping for 5 minutes..."
-    sleep 300  # Wait for 5 minutes (300 seconds)
+    sleep 300 # Wait for 5 minutes (300 seconds)
+
 done
