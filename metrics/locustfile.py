e8597d3d281069b1fd665d555ea769dfa823c525
minhfli <vuvanminh.bacninh@gmail.com>
2025-05-22 15:25:41 +0700

    vertical scaling stack

--- metrics/locustfile.py
+++ metrics/locustfile.py
@@ -2,8 +2,16 @@ from locust import HttpUser, task
 
 
 class MyUser(HttpUser):
-    host = "http://172.24.4.102"
+    host = "http://172.24.4.1"
+    network_timeout = 60
 
     @task
     def burn_cpu(self):
-        self.client.get("/cpu/burn.php")
+        retries = 2
+        while retries > 0:
+            try:
+                self.client.get("/cpu/burn.php", timeout=self.network_timeout)
+                break
+            except Exception as e:
+                print(f"Request failed: {e}")
+                retries -= 1
