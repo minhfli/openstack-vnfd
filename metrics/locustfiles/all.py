5d73d6477015bfa279e2406fb6de547a3d92389b
minhfli <vuvanminh.bacninh@gmail.com>
2025-06-18 22:45:51 +0700

    temp

---
+++ metrics/locustfiles/all.py
@@ -0,0 +1,64 @@
+from locust import FastHttpUser, task
+import locust.stats
+from locust import constant_throughput, constant_pacing, constant, between
+import time
+
+# run with locust
+
+# for stats csv
+locust.stats.PERCENTILES_TO_REPORT = [
+    0.25,
+    0.50,
+    0.65,
+    0.75,
+    0.80,
+    0.90,
+    0.95,
+    0.99,
+    1.00,
+]
+# for UI
+locust.stats.PERCENTILES_TO_CHART = [0.25, 0.50, 0.75, 0.95, 0.99]
+locust.stats.PERCENTILES_TO_STATISTICS = [0.25, 0.50, 0.75, 0.95, 0.99]
+
+
+timeout = 30  # seconds, for all users
+
+wait_time1 = constant_throughput(1)
+wait_time2 = constant_throughput(0.9)
+wait_time3 = constant_throughput(1.1)
+url1 = "/cpu/burn4.php"
+url2 = "/cpu/burn5.php"
+url3 = "/cpu/burn5.php"
+
+# 120 user for vertical scaling test
+
+
+class MyUser1(FastHttpUser):
+    wait_time = wait_time1
+    connection_timeout = timeout
+    network_timeout = timeout
+
+    @task
+    def burn_cpu(self):
+        self.client.get(url1)
+
+
+class MyUser2(FastHttpUser):
+    wait_time = wait_time2
+    connection_timeout = timeout
+    network_timeout = timeout
+
+    @task
+    def burn_cpu(self):
+        self.client.get(url2)
+
+
+class MyUser3(FastHttpUser):
+    wait_time = wait_time3
+    connection_timeout = timeout
+    network_timeout = timeout
+
+    @task
+    def burn_cpu(self):
+        self.client.get(url3)
